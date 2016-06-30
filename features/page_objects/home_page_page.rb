478f785961d3b66be7210fcd62dbc139251d57a7
Martin Holmes <holmesmrtn@aol.com>
2016-06-30 09:30:10 -0400

    rerererefixed stupid feature tests.  All green except comments.

--- features/page_objects/home_page_page.rb
+++ features/page_objects/home_page_page.rb
@@ -3,11 +3,11 @@ require 'page-object'
 class HomePage
   include PageObject
   page_url 'http://localhost:8080/testBlog/'
-  paragraphs(:blog_result_summaries, :xpath => '//div[@class="summary"]/p')
+  divs(:blog_result_summaries, :class => 'summary')
 
   h2(:blog_row1, :id => 'blogId0')
 
-  link(:choose_blog, :xpath => '//h2[@id="blogId0"]/a')
+  link(:choose_blog, :xpath => '//h2[@id="blogId0"]/parent::a')
   text_field(:search_for_blog, :id => 'blogSearch')
   button(:submit, :value => 'Search')
 
