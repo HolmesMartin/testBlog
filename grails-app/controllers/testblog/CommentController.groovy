302d3d7fa128dfb7f2a44587fe9f89c0033dd143
Martin Holmes <holmesmrtn@aol.com>
2016-07-01 14:42:16 -0400

    COMMENTS WORK

--- grails-app/controllers/testblog/CommentController.groovy
+++ grails-app/controllers/testblog/CommentController.groovy
@@ -1,17 +1,29 @@
 package testblog
 
+import grails.converters.JSON
+
 class CommentController {
 
-	def scaffold = true
+	def scaffold = Comment
 
     def index() { }
     
-    def fetchController = {
-		def Comments = Comment.findAllByEntryId("%${params.value}%")
-		render(view:"index", model: [value: params.value, Comments: Comments])
+    def fetchComments = {
+		def realEntry = Entry.findById("${params.entry}".toInteger())
+		def comments = Comment.findAllByEntry(realEntry)
+		render(template:'results', model: [value: params.value, Comments: comments])
 	   }
 	   
 	def save() {
-		//TODO
+		def realEntry = Entry.findById("${params.entry}".toInteger())
+		def commentInstance = new Comment(params)
+		commentInstance.entry = realEntry
+        if (!commentInstance.save(flush: true)) {
+            render(view: "create", model: [commentInstance: commentInstance])
+            return
+        }
+
+		flash.message = message(code: 'default.created.message', args: [message(code: 'comment.label', default: 'Comment'), commentInstance.id])
+		redirect(controller:"entry", action:"show", id:"${params.entry}".toInteger())
 	}
 }
