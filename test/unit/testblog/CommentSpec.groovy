214daafd7c36021765e1a9ca4fb316f161dedc37
Martin Holmes <holmesmrtn@aol.com>
2016-07-05 15:12:34 -0400

    domain unit tests added

--- test/unit/testblog/CommentSpec.groovy
+++ test/unit/testblog/CommentSpec.groovy
@@ -2,19 +2,36 @@ package testblog
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import testblog.Entry
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Comment)
 class CommentSpec extends Specification {
+	
+	def comment
+	
+	def setup() {
 
-    def setup() {
-    }
+	}
 
-    def cleanup() {
-    }
+	def cleanup() {
+	}
 
-    void "test something"() {
-    }
+	void "test good comment constraints"() {
+		when: "test comment is made"
+			def entry = new Entry(title:'test',summary:'test',author:'test')
+			entry.save()
+			String content = 'This is a test comment, this should be just a test.'
+			String author = 'whomsoever'
+			comment = new Comment(author:author, content:content, entry:entry)
+			comment.save()
+		
+		then: "test comment should match"
+			comment.author == author
+			comment.content == content
+			comment.entry == entry
+	}
+
 }
